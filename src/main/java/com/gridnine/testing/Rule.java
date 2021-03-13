525e690b9b9db7219b4f78f895b53475b6c6b6ad
agivdel <agph@yandex.ru>
2021-03-13 09:20:59 +0300

    add interface for future flight filtering rules

---
+++ src/main/java/com/gridnine/testing/Rule.java
@@ -0,0 +1,15 @@
+package com.gridnine.testing;
+
+import java.util.Objects;
+
+public interface Rule<IN, OUT> {
+    default <OUT2> Rule<IN, OUT2> andThen(Rule<OUT, OUT2> next) {
+        Objects.requireNonNull(next);
+        return in -> {
+            OUT out1 = Rule.this.filter(in);
+            return next.filter(out1);
+        };
+    }
+
+    OUT filter(IN in);
+}
