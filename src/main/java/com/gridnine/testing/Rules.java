97897b1da544fb02e49876dad25e54a0ee047282
agivdel <agph@yandex.ru>
2021-03-13 12:22:10 +0300

    rewrite method filterFlightWithDepInPast() with iterator

--- src/main/java/com/gridnine/testing/Rules.java
+++ src/main/java/com/gridnine/testing/Rules.java
@@ -1,10 +1,11 @@
 package com.gridnine.testing;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 
 import static java.util.stream.Collectors.toList;
 
@@ -12,28 +13,24 @@ public class Rules {
     public static final LocalDateTime now = LocalDateTime.now();
 
     /**1) фильтрование полетов с вылетами в будущем: вариант №1*/
-    public List<Flight> filterFlight(List<Flight> flights) {
+    public List<Flight> filterFlightWithDepInPast(List<Flight> flights) {
         return departureInPast.filter(flights);
     }
 
     private final Rule<List<Flight>, List<Flight>> departureInPast = new Rule<List<Flight>, List<Flight>>() {
         @Override
         public List<Flight> filter(List<Flight> flights) {
-            List<Flight> newFlights = new ArrayList<>();
-            final List<Flight> oldFlights = new ArrayList<>(flights);
-            for (Flight oldFlight : oldFlights) {
-                List<Segment> newSegments = new ArrayList<>();
-                List<Segment> oldSegments = oldFlight.getSegments();
-                for (Segment oldSegment : oldSegments) {
-                    if (oldSegment.getDepartureDate().isAfter(now)) {
-                        newSegments.add(oldSegment);
+            List<Flight> resultFlights = new ArrayList<>(flights);
+            Iterator<Flight> iterator = resultFlights.iterator();
+            while (iterator.hasNext()) {
+                Flight flight = iterator.next();
+                for (Segment segment : flight.getSegments()) {
+                    if (segment.getDepartureDate().isBefore(now)) {
+                        iterator.remove();
                     }
                 }
-                if (!newSegments.isEmpty()) {
-                    newFlights.add(new Flight(newSegments));
-                }
             }
-            return newFlights;
+            return resultFlights;
         }
     };
 
