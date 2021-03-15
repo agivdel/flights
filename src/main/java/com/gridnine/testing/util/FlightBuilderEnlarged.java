a5106ee8b0ab9242c2f0ba2590b18bfaf08c08c6
agivdel <agph@yandex.ru>
2021-03-15 11:34:25 +0300

    rewrite main method for flight list building

--- src/main/java/com/gridnine/testing/util/FlightBuilderEnlarged.java
+++ src/main/java/com/gridnine/testing/util/FlightBuilderEnlarged.java
@@ -9,36 +9,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FlightBuilderEnlarged {
-    public static List<Flight> createFlights() {
-        LocalDateTime threeDaysFromNow = LocalDateTime.now().plusDays(3);
-        return Arrays.asList(
-                //A normal flight with two hour duration
-                createFlight(
-                        threeDaysFromNow, threeDaysFromNow.plusHours(2)),
-                //A normal multi segment flight
-                createFlight(
-                        threeDaysFromNow, threeDaysFromNow.plusHours(2),
-                        threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(5)),
-                //A normal multi segment flight with ground time less 1 hours
-                createFlight(
-                        threeDaysFromNow, threeDaysFromNow.plusMinutes(120),
-                        threeDaysFromNow.plusMinutes(165), threeDaysFromNow.plusHours(5)),
-                //A flight departing in the past
-                createFlight(
-                        threeDaysFromNow.minusDays(6), threeDaysFromNow),
-                //A flight that departs after it arrives
-                createFlight(
-                        threeDaysFromNow, threeDaysFromNow.minusHours(6)),
-                //A flight with one hour ground time
-                createFlight(
-                        threeDaysFromNow, threeDaysFromNow.plusHours(2),
-                        threeDaysFromNow.plusHours(5), threeDaysFromNow.plusHours(6)),
-                //Another flight with more than five hours ground time
-                createFlight(
-                        threeDaysFromNow, threeDaysFromNow.plusHours(2),
-                        threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4),
-                        threeDaysFromNow.plusHours(9), threeDaysFromNow.plusHours(10))
-        );
+    private static final LocalDateTime threeDaysFromNow = LocalDateTime.now().plusDays(3);
+
+    public static final Flight normalFlight = createFlight(
+            threeDaysFromNow, threeDaysFromNow.plusHours(2));
+    public static final Flight normalMultiSegmentFlight = createFlight(
+            threeDaysFromNow, threeDaysFromNow.plusHours(2),
+            threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(5));
+    public static final Flight less1HourGroundTimeFlight = createFlight(
+            threeDaysFromNow, threeDaysFromNow.plusMinutes(120),
+            threeDaysFromNow.plusMinutes(165), threeDaysFromNow.plusHours(5));
+    public static final Flight departingInPastFlight = createFlight(
+            threeDaysFromNow.minusDays(6), threeDaysFromNow);
+    public static final Flight departureAfterArrivalFlight = createFlight(
+            threeDaysFromNow, threeDaysFromNow.minusHours(6));
+    public static final Flight threeHoursGroundTimeFlight = createFlight(
+            threeDaysFromNow, threeDaysFromNow.plusHours(2),
+            threeDaysFromNow.plusHours(5), threeDaysFromNow.plusHours(6));
+    public static final Flight sixHoursGroundTimeFlight = createFlight(
+            threeDaysFromNow, threeDaysFromNow.plusHours(2),
+            threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4),
+            threeDaysFromNow.plusHours(9), threeDaysFromNow.plusHours(10));
+
+    public static List<Flight> createFlights(Flight...flights) {
+        return Arrays.asList(flights);
     }
 
     private static Flight createFlight(final LocalDateTime... dates) {
