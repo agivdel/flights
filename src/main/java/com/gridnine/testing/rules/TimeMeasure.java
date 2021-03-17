b61aff6c0ca0b24cd4cf82f69987857648a84591
agivdel <agph@yandex.ru>
2021-03-17 11:06:44 +0300

    add comment

--- src/main/java/com/gridnine/testing/rules/TimeMeasure.java
+++ src/main/java/com/gridnine/testing/rules/TimeMeasure.java
@@ -1,5 +1,8 @@
 package com.gridnine.testing.rules;
 
+/**
+ * The unit of measurement of time is millisecond.
+ * One minute is equals 60000 ms, one hours - 3600000 ms.*/
 public enum TimeMeasure {
     HOURS(3600000),
     MINUTES(60000);
